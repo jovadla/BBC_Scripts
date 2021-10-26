3a8e8f28c0eefcca7ba4906167343c4d2f376849
jyothi <vjyothirmayorao@gmail.com>
2021-10-26 18:07:22 +0200

    fs recon script used on Monster

---
+++ fs_recon-all.sh
@@ -0,0 +1,32 @@
+You might need to start the docker daemon: sudo docker
+```bash
+#!/bin/bash
+
+# set pwd = cd /mnt/data/jyothi/fmriprep/
+## freesurfer licence
+sudo systemctl start docker
+freesurfer_license=~/Documents/fs_license/license.txt
+
+### PARTICIPANTS 
+
+## for one participant
+#participants_labels=blndTRE028
+
+## for several subjects filter according to a pattern
+pattern=inputs/raw/sub-*COP[01][0123456789]
+participants_labels=ls -d $pattern | sed s@inputs/raw/sub-@@g
+
+# This will run fMRIprep anat mode only
+bids_dir='pwd'/inputs/raw
+output_dir='pwd'/derivatives
+mkdir -p $output_dir
+docker run -it --rm \
+  -v $freesurfer_license:/license.txt \
+  -v $bids_dir:/data:ro \
+  -v $output_dir:/out \
+  nipreps/fmriprep:20.2.3 /data/ /out/ \
+  participant --participant_label $participants_labels \
+  --fs-license-file /license.txt \
+  --nprocs 12 --nthreads 12 --n_cpus 12 --n-cpus 12 \
+  --output-spaces T1w:res-native MNI152NLin2009cAsym:res-native fsaverage\
+  --anat-only
